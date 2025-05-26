4a8c7df20566535c907ea92743ea6b94b01723df
cdiaz21 <50161435+cdiaz21@users.noreply.github.com>
2025-05-26 03:58:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import os
+# Variables de entorno para Blogger
+blogger_credentials_json = os.getenv("BLOGGER_CREDENTIALS_JSON")
+
+# Variables de entorno para Bluesky
+bluesky_handle = os.getenv("BLUESKY_HANDLE")
+bluesky_app_password = os.getenv("BLUESKY_APP_PASSWORD")
+
+# Variables de entorno para Telegram
+telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+telegram_chat_id = os.getenv("TELEGRAM_CHAT_ID")
+if not blogger_credentials_json:
+    print("Error: Faltan las credenciales de Blogger.")
+
+if not bluesky_handle or not bluesky_app_password:
+    print("Error: Faltan las credenciales de Bluesky.")
+
+if not telegram_bot_token or not telegram_chat_id:
+    print("Faltan variables de entorno para Telegram.")
+import os
 from modules.noticias import obtener_noticias
 from modules.resumen import generar_resumen
 from modules.telegram import publicar_en_telegram
